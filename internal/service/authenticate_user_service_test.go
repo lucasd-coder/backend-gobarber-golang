d81132c6e7ae6a3dd4969b4b0488a003c9e0d702
lucas <lucasdasilva68@yahoo.com>
2022-04-21 18:18:54 -0300

    add tests

---
+++ internal/service/authenticate_user_service_test.go
@@ -0,0 +1,70 @@
+package service_test
+
+import (
+	"testing"
+
+	"backend-gobarber-golang/internal/dtos"
+	"backend-gobarber-golang/internal/model"
+	"backend-gobarber-golang/internal/service"
+	"backend-gobarber-golang/internal/test"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAuthenticateUserService_AuthenticateFailNonExistingUser(t *testing.T) {
+	mockRepo := new(test.MockUserRepository)
+	jwtService := service.JWTService{}
+
+	email := "lucas@gmail.com"
+
+	mockRepo.On("FindByEmail", email).Return(&model.User{})
+
+	userDto := dtos.Credentials{Email: email, Password: "123456"}
+
+	testService := service.AuthenticateUserService{mockRepo, &jwtService}
+
+	_, err := testService.Execute(&userDto)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "Incorrect email/password combination.", err.Error())
+}
+
+func TestAuthenticateUserService_AuthenticateFailWithWrongPassword(t *testing.T) {
+	mockRepo := new(test.MockUserRepository)
+	jwtService := service.JWTService{}
+
+	var id uuid.UUID = uuid.MustParse("0399e631-e2f0-4df5-b1d0-ca6d567a318c")
+	user := model.User{ID: id, Name: "lucas", Email: "lucas@gmail.com", Password: "123456"}
+
+	mockRepo.On("FindByEmail", user.Email).Return(&user)
+
+	userDto := dtos.Credentials{Email: user.Email, Password: "wrong-password"}
+
+	testService := service.AuthenticateUserService{mockRepo, &jwtService}
+
+	_, err := testService.Execute(&userDto)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "Incorrect email/password combination.", err.Error())
+}
+
+func TestAuthenticateUserService_AuthenticateSuccessfully(t *testing.T) {
+	mockRepo := new(test.MockUserRepository)
+	jwtService := service.JWTService{}
+
+	var id uuid.UUID = uuid.MustParse("0399e631-e2f0-4df5-b1d0-ca6d567a318c")
+	user := model.User{ID: id, Name: "lucas", Email: "lucas@gmail.com", Password: "$2a$08$7xWE3NGEXhnKHYi7wcFUw.wMDtisIPK4T78lmjnSOsYEO.6gTuy1W"}
+
+	mockRepo.On("FindByEmail", user.Email).Return(&user)
+
+	userDto := dtos.Credentials{Email: user.Email, Password: "123456"}
+
+	testService := service.AuthenticateUserService{mockRepo, &jwtService}
+
+	resp, err := testService.Execute(&userDto)
+
+	assert.Nil(t, err)
+	assert.NotEmpty(t, resp.Response)
+	assert.NotEmpty(t, resp.Token)
+}
---
+++ internal/service/jwt_service_test.go
@@ -0,0 +1,45 @@
+package service_test
+
+import (
+	"testing"
+
+	"backend-gobarber-golang/internal/service"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJWTService_GenerateTokenSuccessfully(t *testing.T) {
+	var id uuid.UUID = uuid.MustParse("0399e631-e2f0-4df5-b1d0-ca6d567a318c")
+
+	testService := service.JWTService{}
+
+	token := testService.GenerateToken(id.String())
+
+	assert.NotNil(t, token)
+}
+
+func TestJWTService_ValidateTokenSuccessfully(t *testing.T) {
+	var id uuid.UUID = uuid.MustParse("0399e631-e2f0-4df5-b1d0-ca6d567a318c")
+
+	testService := service.JWTService{}
+
+	token := testService.GenerateToken(id.String())
+
+	resp, err := testService.ValidateToken(token)
+
+	assert.Nil(t, err)
+	assert.True(t, resp.Valid)
+}
+
+func TestJWTService_ErrorUnauthorizedValidateToken(t *testing.T) {
+	testService := service.JWTService{}
+
+	var testToken string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG" +
+		"4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+
+	resp, err := testService.ValidateToken(testToken)
+
+	assert.NotNil(t, err)
+	assert.False(t, resp.Valid)
+}
